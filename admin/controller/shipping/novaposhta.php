0ca47402cc49c68c9421101532c9be391241c3e6
trimal2 <trimal@mail.ru>
2016-02-16 22:54:07 +0200

    update for opencart 2 support
    updated code for support opencart 2 but got 500 error
    can you help me please to let it run on opencart 2

--- admin/controller/shipping/novaposhta.php
+++ admin/controller/shipping/novaposhta.php
@@ -13,6 +13,8 @@ class ControllerShippingNovaPoshta extends Controller
         $this->document->setTitle($this->language->get('heading_title'));
 
         $this->load->model('setting/setting');
+		
+		$data = array();
 
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
             $this->model_setting_setting->editSetting('novaposhta', $this->request->post);
@@ -22,157 +24,157 @@ class ControllerShippingNovaPoshta extends Controller
             $this->redirect($this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'));
         }
 
-        $this->data['token'] = $this->session->data['token'];
+        $data['token'] = $this->session->data['token'];
 
-        $this->data['heading_title'] = $this->language->get('heading_title');
+        $data['heading_title'] = $this->language->get('heading_title');
 
-        $this->data['text_enabled'] = $this->language->get('text_enabled');
-        $this->data['text_disabled'] = $this->language->get('text_disabled');
-        $this->data['text_all_zones'] = $this->language->get('text_all_zones');
-        $this->data['text_none'] = $this->language->get('text_none');
-        $this->data['text_select'] = $this->language->get('text_select');
+        $data['text_enabled'] = $this->language->get('text_enabled');
+        $data['text_disabled'] = $this->language->get('text_disabled');
+        $data['text_all_zones'] = $this->language->get('text_all_zones');
+        $data['text_none'] = $this->language->get('text_none');
+        $data['text_select'] = $this->language->get('text_select');
 
-        $this->data['entry_cost'] = $this->language->get('entry_cost');
-        $this->data['entry_sender_warehouse'] = $this->language->get('entry_sender_warehouse');
-        $this->data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
-        $this->data['entry_status'] = $this->language->get('entry_status');
-        $this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
-        $this->data['entry_api_key'] = $this->language->get('entry_api_key');
-        $this->data['entry_sender_city'] = $this->language->get('entry_sender_city');
-        $this->data['entry_comment'] = $this->language->get('entry_comment');
-        $this->data['entry_send_order_status'] = $this->language->get('entry_send_order_status');
-        $this->data['entry_sender_organization'] = $this->language->get('entry_sender_organization');
-        $this->data['entry_sender_person'] = $this->language->get('entry_sender_person');
-        $this->data['entry_sender_phone'] = $this->language->get('entry_sender_phone');
+        $data['entry_cost'] = $this->language->get('entry_cost');
+        $data['entry_sender_warehouse'] = $this->language->get('entry_sender_warehouse');
+        $data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
+        $data['entry_status'] = $this->language->get('entry_status');
+        $data['entry_sort_order'] = $this->language->get('entry_sort_order');
+        $data['entry_api_key'] = $this->language->get('entry_api_key');
+        $data['entry_sender_city'] = $this->language->get('entry_sender_city');
+        $data['entry_comment'] = $this->language->get('entry_comment');
+        $data['entry_send_order_status'] = $this->language->get('entry_send_order_status');
+        $data['entry_sender_organization'] = $this->language->get('entry_sender_organization');
+        $data['entry_sender_person'] = $this->language->get('entry_sender_person');
+        $data['entry_sender_phone'] = $this->language->get('entry_sender_phone');
 
-        $this->data['button_save'] = $this->language->get('button_save');
-        $this->data['button_cancel'] = $this->language->get('button_cancel');
+        $data['button_save'] = $this->language->get('button_save');
+        $data['button_cancel'] = $this->language->get('button_cancel');
 
         if (isset($this->error['warning'])) {
-            $this->data['error_warning'] = $this->error['warning'];
+            $data['error_warning'] = $this->error['warning'];
         } else {
-            $this->data['error_warning'] = '';
+            $data['error_warning'] = '';
         }
 
-        $this->data['breadcrumbs'] = array();
+        $data['breadcrumbs'] = array();
 
-        $this->data['breadcrumbs'][] = array(
+        $data['breadcrumbs'][] = array(
             'text' => $this->language->get('text_home'),
             'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
             'separator' => false
         );
 
-        $this->data['breadcrumbs'][] = array(
+        $data['breadcrumbs'][] = array(
             'text' => $this->language->get('text_shipping'),
             'href' => $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'),
             'separator' => ' :: '
         );
 
-        $this->data['breadcrumbs'][] = array(
+        $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title'),
             'href' => $this->url->link('shipping/novaposhta', 'token=' . $this->session->data['token'], 'SSL'),
             'separator' => ' :: '
         );
 
-        $this->data['action'] = $this->url->link('shipping/novaposhta', 'token=' . $this->session->data['token'], 'SSL');
+        $data['action'] = $this->url->link('shipping/novaposhta', 'token=' . $this->session->data['token'], 'SSL');
 
-        $this->data['cancel'] = $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL');
+        $data['cancel'] = $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL');
 
         if (isset($this->request->post['novaposhta_api_key'])) {
-            $this->data['novaposhta_api_key'] = $this->request->post['novaposhta_api_key'];
+            $data['novaposhta_api_key'] = $this->request->post['novaposhta_api_key'];
         } else {
-            $this->data['novaposhta_api_key'] = $this->config->get('novaposhta_api_key');
+            $data['novaposhta_api_key'] = $this->config->get('novaposhta_api_key');
         }
 
         if (isset($this->request->post['novaposhta_sender_organization'])) {
-            $this->data['novaposhta_sender_organization'] = $this->request->post['novaposhta_sender_organization'];
+            $data['novaposhta_sender_organization'] = $this->request->post['novaposhta_sender_organization'];
         } else {
-            $this->data['novaposhta_sender_organization'] = $this->config->get('novaposhta_sender_organization');
+            $data['novaposhta_sender_organization'] = $this->config->get('novaposhta_sender_organization');
         }
 
         if (isset($this->request->post['novaposhta_sender_person'])) {
-            $this->data['novaposhta_sender_person'] = $this->request->post['novaposhta_sender_person'];
+            $data['novaposhta_sender_person'] = $this->request->post['novaposhta_sender_person'];
         } else {
-            $this->data['novaposhta_sender_person'] = $this->config->get('novaposhta_sender_person');
+            $data['novaposhta_sender_person'] = $this->config->get('novaposhta_sender_person');
         }
 
         if (isset($this->request->post['novaposhta_sender_phone'])) {
-            $this->data['novaposhta_sender_phone'] = $this->request->post['novaposhta_sender_phone'];
+            $data['novaposhta_sender_phone'] = $this->request->post['novaposhta_sender_phone'];
         } else {
-            $this->data['novaposhta_sender_phone'] = $this->config->get('novaposhta_sender_phone');
+            $data['novaposhta_sender_phone'] = $this->config->get('novaposhta_sender_phone');
         }
 
         if (isset($this->request->post['novaposhta_geo_zone_id'])) {
-            $this->data['novaposhta_geo_zone_id'] = $this->request->post['novaposhta_geo_zone_id'];
+            $data['novaposhta_geo_zone_id'] = $this->request->post['novaposhta_geo_zone_id'];
         } else {
-            $this->data['novaposhta_geo_zone_id'] = $this->config->get('novaposhta_geo_zone_id');
+            $data['novaposhta_geo_zone_id'] = $this->config->get('novaposhta_geo_zone_id');
         }
 
         if (isset($this->request->post['novaposhta_comment'])) {
-            $this->data['novaposhta_comment'] = $this->request->post['novaposhta_comment'];
+            $data['novaposhta_comment'] = $this->request->post['novaposhta_comment'];
         } else {
-            $this->data['novaposhta_comment'] = $this->config->get('novaposhta_comment');
+            $data['novaposhta_comment'] = $this->config->get('novaposhta_comment');
         }
 
 
         if (isset($this->request->post['novaposhta_sender_city'])) {
-            $this->data['novaposhta_sender_city'] = $this->request->post['novaposhta_sender_city'];
+            $data['novaposhta_sender_city'] = $this->request->post['novaposhta_sender_city'];
         } else {
-            $this->data['novaposhta_sender_city'] = $this->config->get('novaposhta_sender_city');
+            $data['novaposhta_sender_city'] = $this->config->get('novaposhta_sender_city');
         }
 
         if (isset($this->request->post['novaposhta_sender_city_name'])) {
-            $this->data['novaposhta_sender_city_name'] = $this->request->post['novaposhta_sender_city_name'];
+            $data['novaposhta_sender_city_name'] = $this->request->post['novaposhta_sender_city_name'];
         } else {
-            $this->data['novaposhta_sender_city_name'] = $this->config->get('novaposhta_sender_city_name');
+            $data['novaposhta_sender_city_name'] = $this->config->get('novaposhta_sender_city_name');
         }
 
         if (isset($this->request->post['novaposhta_status'])) {
-            $this->data['novaposhta_status'] = $this->request->post['novaposhta_status'];
+            $data['novaposhta_status'] = $this->request->post['novaposhta_status'];
         } else {
-            $this->data['novaposhta_status'] = $this->config->get('novaposhta_status');
+            $data['novaposhta_status'] = $this->config->get('novaposhta_status');
         }
 
         if (isset($this->request->post['novaposhta_sort_order'])) {
-            $this->data['novaposhta_sort_order'] = $this->request->post['novaposhta_sort_order'];
+            $data['novaposhta_sort_order'] = $this->request->post['novaposhta_sort_order'];
         } else {
-            $this->data['novaposhta_sort_order'] = $this->config->get('novaposhta_sort_order');
+            $data['novaposhta_sort_order'] = $this->config->get('novaposhta_sort_order');
         }
 
         $this->load->model('localisation/order_status');
-        $this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+        $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
 
         if (isset($this->request->post['novaposhta_send_order_status'])) {
-            $this->data['novaposhta_send_order_status'] = $this->request->post['novaposhta_send_order_status'];
+            $data['novaposhta_send_order_status'] = $this->request->post['novaposhta_send_order_status'];
         } else {
-            $this->data['novaposhta_send_order_status'] = $this->config->get('novaposhta_send_order_status');
+            $data['novaposhta_send_order_status'] = $this->config->get('novaposhta_send_order_status');
         }
 
         $this->load->model('localisation/geo_zone');
 
-        $this->data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+        $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 
         $this->load->model('localisation/zone');
 
-        $this->data['zones'] = $this->model_localisation_zone->getZones();
+        $data['zones'] = $this->model_localisation_zone->getZones();
         $this->load->model('localisation/language');
-        $this->data['languages'] = $this->model_localisation_language->getLanguages();
+        $data['languages'] = $this->model_localisation_language->getLanguages();
 
 
         if (isset($this->request->post['novaposhta_sender_warehouse'])) {
-            $this->data['novaposhta_sender_warehouse'] = $this->request->post['novaposhta_sender_warehouse'];
+            $data['novaposhta_sender_warehouse'] = $this->request->post['novaposhta_sender_warehouse'];
         } else {
-            $this->data['novaposhta_sender_warehouse'] = $this->config->get('novaposhta_sender_warehouse');
+            $data['novaposhta_sender_warehouse'] = $this->config->get('novaposhta_sender_warehouse');
         }
 
 
-        $this->template = 'shipping/novaposhta.tpl';
+        template = 'shipping/novaposhta.tpl';
         $this->children = array(
             'common/header',
             'common/footer'
         );
 
-        $this->response->setOutput($this->render());
+        $this->response->setOutput($this->load->view($data));
     }
 
     public function getCities()
